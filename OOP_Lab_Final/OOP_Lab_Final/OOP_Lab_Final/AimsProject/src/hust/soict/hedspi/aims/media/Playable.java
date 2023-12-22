a17775837ffa4691e25e38d125872a86d32e89cf
3ngk1sha <108885735+3ngk1sha@users.noreply.github.com>
2023-12-22 22:34:52 +0700

    Lab05_OOP

---
+++ OOP_Lab_Final/OOP_Lab_Final/OOP_Lab_Final/AimsProject/src/hust/soict/hedspi/aims/media/Playable.java
@@ -0,0 +1,8 @@
+//NguyenVanDung20215013
+package hust.soict.hedspi.aims.media;
+
+import hust.soict.hedspi.aims.exception.PlayerException;
+
+public interface Playable {
+	public void play() throws PlayerException;
+}
